ddfa29cc0b749775cf73475dc46b3bf1d4cba8ca
skwas <skwas@skwas.net>
2019-03-13 04:31:10 +0100

    Add docs

--- src/Correlate.Abstractions/ICorrelationContextFactory.cs
+++ src/Correlate.Abstractions/ICorrelationContextFactory.cs
@@ -1,9 +1,20 @@
 ﻿namespace Correlate.Abstractions
 {
+	/// <summary>
+	/// Describes how to create/clean up a <see cref="CorrelationContext"/>.
+	/// </summary>
 	public interface ICorrelationContextFactory
 	{
+		/// <summary>
+		/// Creates a new <see cref="CorrelationContext"/>.
+		/// </summary>
+		/// <param name="correlationId">The correlation id to associate to the context.</param>
+		/// <returns>The <see cref="CorrelationContext"/> containing the correlation id.</returns>
 		CorrelationContext Create(string correlationId);
 
+		/// <summary>
+		/// Disposes the <see cref="CorrelationContext"/>.
+		/// </summary>
 		void Dispose();
 	}
 }
--- src/Correlate/CorrelationContextFactory.cs
+++ src/Correlate/CorrelationContextFactory.cs
@@ -3,20 +3,30 @@ using System;
 
 namespace Correlate
 {
+	/// <summary>
+	/// Factory to create/clean up a <see cref="CorrelationContext"/> and optionally associate it with a <see cref="ICorrelationContextAccessor"/>.
+	/// </summary>
 	public class CorrelationContextFactory : ICorrelationContextFactory
 	{
 		private readonly ICorrelationContextAccessor _correlationContextAccessor;
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CorrelationContextFactory"/> class.
+		/// </summary>
 		public CorrelationContextFactory()
 		{
 		}
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CorrelationContextFactory"/> class using specified context accessor.
+		/// </summary>
 		public CorrelationContextFactory(ICorrelationContextAccessor correlationContextAccessor)
 			: this()
 		{
 			_correlationContextAccessor = correlationContextAccessor ?? throw new ArgumentNullException(nameof(correlationContextAccessor));
 		}
 
+		/// <inheritdoc />
 		CorrelationContext ICorrelationContextFactory.Create(string correlationId)
 		{
 			CorrelationContext correlationContext = Create(correlationId);
@@ -28,6 +38,11 @@ namespace Correlate
 			return correlationContext;
 		}
 
+		/// <summary>
+		/// Creates a new <see cref="CorrelationContext"/>.
+		/// </summary>
+		/// <param name="correlationId">The correlation id to associate to the context.</param>
+		/// <returns>The <see cref="CorrelationContext"/> containing the correlation id.</returns>
 		public virtual CorrelationContext Create(string correlationId)
 		{
 			return new CorrelationContext
@@ -36,6 +51,7 @@ namespace Correlate
 			};
 		}
 
+		/// <inheritdoc />
 		public void Dispose()
 		{
 			if (_correlationContextAccessor != null)
