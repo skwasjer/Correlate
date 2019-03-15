414fac12ae8c2ea6eb1df90ae7e8441f6b0ce845
skwas <skwas@skwas.net>
2019-03-15 01:49:47 +0100

    Move CorrelateOptions to AspNetCore lib, since it's specific options to the middleware. Also requires moving some of the DI extensions.

--- src/Correlate.AspNetCore/AspNetCore/Middleware/CorrelateOptions.cs
+++ src/Correlate.AspNetCore/AspNetCore/Middleware/CorrelateOptions.cs
@@ -1,4 +1,4 @@
-﻿namespace Correlate
+﻿namespace Correlate.AspNetCore.Middleware
 {
 	/// <summary>
 	/// Options for handling correlation id on incoming requests.
--- src/Correlate.AspNetCore/DependencyInjection/IServiceCollectionExtensions.cs
+++ src/Correlate.AspNetCore/DependencyInjection/IServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
+﻿using System;
+using Correlate.AspNetCore.Middleware;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Correlate.DependencyInjection
 {
@@ -13,15 +15,13 @@ namespace Correlate.DependencyInjection
 		/// Adds services required for using correlation.
 		/// </summary>
 		/// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+		/// <param name="configureOptions">The action used to configure <see cref="CorrelateOptions"/>.</param>
 		/// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
-		public static IServiceCollection AddCorrelate(this IServiceCollection services)
+		public static IServiceCollection AddCorrelate(this IServiceCollection services, Action<CorrelateOptions> configureOptions)
 		{
-			services.TryAddSingleton<ICorrelationContextAccessor, CorrelationContextAccessor>();
-			services.TryAddTransient<ICorrelationContextFactory, CorrelationContextFactory>();
-			services.TryAddSingleton<ICorrelationIdFactory, GuidCorrelationIdFactory>();
-			services.TryAddSingleton<CorrelationManager>();
+			services.Configure(configureOptions);
 
-			return services;
+			return services.AddCorrelate();
 		}
 	}
 }
