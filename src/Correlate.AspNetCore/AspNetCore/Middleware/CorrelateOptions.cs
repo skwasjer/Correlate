42479928cab3fef9803ac21b47468361e559c813
skwas <skwas@skwas.net>
2019-07-03 18:36:23 +0200

    Clarify use case of IncludeInResponse option

--- src/Correlate.AspNetCore/AspNetCore/Middleware/CorrelateOptions.cs
+++ src/Correlate.AspNetCore/AspNetCore/Middleware/CorrelateOptions.cs
@@ -20,6 +20,9 @@ namespace Correlate.AspNetCore.Middleware
 		/// <summary>
 		/// Gets or sets whether to include the correlation id in the response.
 		/// </summary>
+		/// <remarks>
+		/// A common use case is to disable tracing info in edge services, so that such details are not exposed to the outside world.
+		/// </remarks>
 		public bool IncludeInResponse { get; set; } = true;
 	}
 }
