9dbd721ceeb75add711e1ca795b4d8a5d32aba20
skwas <skwas@skwas.net>
2019-07-23 11:57:31 +0200

    Add async result overloads

--- src/Correlate.Core/IAsyncCorrelationManagerExtensions.cs
+++ src/Correlate.Core/IAsyncCorrelationManagerExtensions.cs
@@ -51,5 +51,49 @@ namespace Correlate
 		{
 			return asyncCorrelationManager.CorrelateAsync(correlationId, correlatedTask, null);
 		}
+
+		/// <summary>
+		/// Executes the <paramref name="correlatedTask"/> with its own <see cref="CorrelationContext"/>.
+		/// </summary>
+		/// <param name="asyncCorrelationManager">The async correlation manager.</param>
+		/// <param name="correlatedTask">The task to execute.</param>
+		/// <returns>An awaitable that completes once the <paramref name="correlatedTask"/> has executed and the correlation context has disposed.</returns>
+		/// <remarks>
+		/// When logging and tracing are both disabled, no correlation context is created and the task simply executed as it normally would.
+		/// </remarks>
+		public static Task<T> CorrelateAsync<T>(this IAsyncCorrelationManager asyncCorrelationManager, Func<Task<T>> correlatedTask)
+		{
+			return asyncCorrelationManager.CorrelateAsync(null, correlatedTask);
+		}
+
+		/// <summary>
+		/// Executes the <paramref name="correlatedTask"/> with its own <see cref="CorrelationContext"/>.
+		/// </summary>
+		/// <param name="asyncCorrelationManager">The async correlation manager.</param>
+		/// <param name="correlatedTask">The task to execute.</param>
+		/// <param name="onException">A delegate to handle the exception inside the correlation scope, before it is disposed. Returns <see langword="true" /> to consider the exception handled, or <see langword="false" /> to throw.</param>
+		/// <returns>An awaitable that completes once the <paramref name="correlatedTask"/> has executed and the correlation context has disposed.</returns>
+		/// <remarks>
+		/// When logging and tracing are both disabled, no correlation context is created and the task simply executed as it normally would.
+		/// </remarks>
+		public static Task<T> CorrelateAsync<T>(this IAsyncCorrelationManager asyncCorrelationManager, Func<Task<T>> correlatedTask, OnException onException)
+		{
+			return asyncCorrelationManager.CorrelateAsync(null, correlatedTask, onException);
+		}
+
+		/// <summary>
+		/// Executes the <paramref name="correlatedTask"/> with its own <see cref="CorrelationContext"/>.
+		/// </summary>
+		/// <param name="asyncCorrelationManager">The async correlation manager.</param>
+		/// <param name="correlationId">The correlation id to use, or null to generate a new one.</param>
+		/// <param name="correlatedTask">The task to execute.</param>
+		/// <returns>An awaitable that completes once the <paramref name="correlatedTask"/> has executed and the correlation context has disposed.</returns>
+		/// <remarks>
+		/// When logging and tracing are both disabled, no correlation context is created and the task simply executed as it normally would.
+		/// </remarks>
+		public static Task<T> CorrelateAsync<T>(this IAsyncCorrelationManager asyncCorrelationManager, string correlationId, Func<Task<T>> correlatedTask)
+		{
+			return asyncCorrelationManager.CorrelateAsync(correlationId, correlatedTask, null);
+		}
 	}
 }
