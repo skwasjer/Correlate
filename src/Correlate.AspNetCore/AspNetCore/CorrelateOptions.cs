007a7664102de6a4a5f6118291c7eeb1c9d5d159
skwasjer <11424653+skwasjer@users.noreply.github.com>
2023-08-31 22:23:20 +0200

    chore: ensure options is never null or empty and seal the type. Improve xml docs.

--- src/Correlate.AspNetCore/AspNetCore/CorrelateOptions.cs
+++ src/Correlate.AspNetCore/AspNetCore/CorrelateOptions.cs
@@ -5,21 +5,30 @@ namespace Correlate.AspNetCore;
 /// <summary>
 /// Options for handling correlation id on incoming requests.
 /// </summary>
-public class CorrelateOptions
+public sealed class CorrelateOptions
 {
+    private static readonly string[] DefaultRequestHeaders = { CorrelationHttpHeaders.CorrelationId };
+
+    private string[]? _requestHeaders;
+
     /// <summary>
-    /// Gets or sets the request headers to retrieve the correlation id from.
+    /// Gets or sets the request headers to retrieve the correlation id from. Defaults to <c>X-Correlation-ID</c>.
     /// </summary>
     /// <remarks>
-    /// The first matching header will be used.
+    /// The first matching request header will be used.
     /// </remarks>
-    public string[] RequestHeaders { get; set; } = { CorrelationHttpHeaders.CorrelationId };
+    public string[] RequestHeaders
+    {
+        get => _requestHeaders ?? DefaultRequestHeaders;
+        set => _requestHeaders = value;
+    }
 
     /// <summary>
     /// Gets or sets whether to include the correlation id in the response.
+    /// <para>If the correlation id was received in the request, it will use the exact same header name in the response. If the request did not have a correlation id header (matching one in <see cref="RequestHeaders" />), the default response header will be <c>X-Correlation-ID</c>.</para>
     /// </summary>
     /// <remarks>
-    /// A common use case is to disable tracing info in edge services, so that such details are not exposed to the outside world.
+    /// You may want to consider disabling this in edge services, so that tracing details are not exposed to the outside world.
     /// </remarks>
     public bool IncludeInResponse { get; set; } = true;
 }
