b979581ed586d97605f9dd32c21075886acd2fa6
Eric <e-salmon@users.noreply.github.com>
2024-09-08 15:58:02 +0100

    feat: Add ToString override to CorrelatedLogScope (#110)
    * Add ToStirng override to CorrelatedLogScope
    
    Output to the default Console logger uses `ToString` on scope items.
    
    * test: add coverage and simplify with concatenation
    
    ---------
    
    Co-authored-by: skwasjer <11424653+skwasjer@users.noreply.github.com>

---
+++ test/Correlate.Core.Tests/Extensions/LoggerExtensionsTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Correlate.Extensions;
+
+public sealed class LoggerExtensionsTests
+{
+    private readonly ILogger _logger;
+
+    public LoggerExtensionsTests()
+    {
+        _logger = Substitute.For<ILogger>();
+    }
+
+    [Theory]
+    [InlineData(CorrelateConstants.CorrelationIdKey, "12345")]
+    [InlineData("CustomKey", "abcdef")]
+    public void When_beginning_scope_it_should_return_disposable_scope_implementing_kvp_list_containing_the_correlation_id
+    (
+        string scopeKey,
+        string correlationId
+    )
+    {
+        var expectedKvp = new KeyValuePair<string, object>(scopeKey, correlationId);
+
+        Func<IReadOnlyList<KeyValuePair<string, object>>, bool> assertScope = kvps =>
+        {
+            kvps.Should()
+                .ContainSingle()
+                .Which.Should()
+                .Be(expectedKvp);
+            return true;
+        };
+
+        // Act
+        _logger.BeginCorrelatedScope(scopeKey, correlationId);
+
+        // Assert
+        _logger.Received(1).BeginScope(Arg.Is<IReadOnlyList<KeyValuePair<string, object>>>(e => assertScope(e)));
+    }
+
+    [Theory]
+    [InlineData(CorrelateConstants.CorrelationIdKey, "12345")]
+    [InlineData("CustomKey", "abcdef")]
+    public void When_formatting_scope_it_should_return_expected
+    (
+        string scopeKey,
+        string correlationId
+    )
+    {
+        string expectedStr = $"{scopeKey}:{correlationId}";
+
+        Func<object, bool> assertScope = formattable =>
+        {
+            formattable.ToString().Should().Be(expectedStr);
+            return true;
+        };
+
+        // Act
+        _logger.BeginCorrelatedScope(scopeKey, correlationId);
+
+        // Assert
+        _logger.Received(1).BeginScope(Arg.Is<object>(e => assertScope(e)));
+    }
+}
