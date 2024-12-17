9b88773a65b77a5bddfd752614c5b38e248fd7dd
Martijn Bodeman <11424653+skwasjer@users.noreply.github.com>
2024-12-17 18:38:02 +0100

    test: verify public API so we can detect (breaking) changes (#131)

---
+++ test/Correlate.Abstractions.Tests/PublicApiTests.cs
@@ -0,0 +1,11 @@
+﻿using Correlate.Testing.Specs;
+
+namespace Correlate;
+
+public sealed class PublicApiTests : PublicApiSpec
+{
+    public PublicApiTests()
+        : base(typeof(IActivity))
+    {
+    }
+}
