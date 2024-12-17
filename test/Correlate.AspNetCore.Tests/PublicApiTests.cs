0a14ecc9025b28a7b081b0452001ce9c99aeec88
skwasjer <11424653+skwasjer@users.noreply.github.com>
2024-12-17 18:21:12 +0100

    test: verify public API so we can detect (breaking) changes

---
+++ test/Correlate.AspNetCore.Tests/PublicApiTests.cs
@@ -0,0 +1,12 @@
+﻿using Correlate.AspNetCore;
+using Correlate.Testing.Specs;
+
+namespace Correlate;
+
+public sealed class PublicApiTests : PublicApiSpec
+{
+    public PublicApiTests()
+        : base(typeof(ICorrelateFeature))
+    {
+    }
+}
---
+++ test/Correlate.DependencyInjection.Tests/PublicApiTests.cs
@@ -0,0 +1,11 @@
+﻿using Correlate.Testing.Specs;
+
+namespace Correlate.DependencyInjection;
+
+public sealed class PublicApiTests : PublicApiSpec
+{
+    public PublicApiTests()
+        : base(typeof(IServiceCollectionExtensions))
+    {
+    }
+}
