49c0bf0e4ac6a5b58e475fa9a03e9f0010cd84ae
Markus Hartung <markus.hartung@skandikon.se>
2025-06-12 23:43:50 +0100

    Added simple WebApi using Correlate #132

---
+++ apitest/Correlate.WebApiTestNet48/App_Start/FilterConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+using Correlate.Http;
+using Correlate.WebApiTestNet48.ActionFilters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Correlate.WebApiTestNet48;
+
+public static class FilterConfig
+{
+    public static void Register(HttpConfiguration config, ServiceProvider serviceProvider)
+    {
+        config.Filters.Add(new CorrelationIdActionFilter(
+            serviceProvider.GetRequiredService<ICorrelationContextAccessor>(),
+            serviceProvider.GetRequiredService<IOptions<CorrelateClientOptions>>(),
+            serviceProvider.GetRequiredService<IActivityFactory>()));
+    }
+}
---
+++ apitest/Correlate.WebApiTestNet48/DependencyResolver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http.Dependencies;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Correlate.WebApiTestNet48;
+
+public class DependencyResolver(IServiceProvider serviceProvider) : IDependencyResolver
+{
+    private IServiceProvider ServiceProvider { get; } = serviceProvider;
+
+    public IDependencyScope BeginScope() => new DependencyResolver(ServiceProvider.CreateScope().ServiceProvider);
+
+    public void Dispose() => (ServiceProvider as IDisposable)?.Dispose();
+
+    public object GetService(Type serviceType) => ServiceProvider.GetService(serviceType);
+
+    public IEnumerable<object?> GetServices(Type serviceType) => ServiceProvider.GetServices(serviceType);
+}
